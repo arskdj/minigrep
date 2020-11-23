5f8f54d0cae4c8db479b254af8b935b2789bac04
arskdj <68353557+arskdj@users.noreply.github.com>
2020-11-23 15:16:26 +0200

    error output

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
+use minigrep::Config;
 use std::env;
 use std::process;
-use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Parsing args error: {}", err);
+        eprintln!("Parsing args error: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Error: {}", e);
+        eprintln!("Error: {}", e);
     }
 }
